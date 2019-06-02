f0206e11c773f9e59ee43c8678f7eedf8159b8a8
pentiumCM <842679178@qq.com>
2019-06-02 11:12:50 +0800

    index中js,css,html代码分离

---
+++ followme_app/public/js/appconstant.js
@@ -0,0 +1,7 @@
+var tab_titles = ["新动态", "发布", "我的"];
+
+/*顶部title栏*/
+var _titles = ['首页', '消息', '个人中心']
+
+/*底部按钮对应的界面*/
+var subpages = ['home/home.html', 'message/message.html', 'account/me.html'];
