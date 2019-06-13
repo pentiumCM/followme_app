ed651534e49b076e8f3c37c10692e709a29a2716
liuhaiying1 <2825402859@qq.com>
2019-06-13 10:47:22 +0800

    20190613提交详情页面3

--- followme_app/public/js/appconstant.js
+++ followme_app/public/js/appconstant.js
@@ -1,6 +1,6 @@
 /*app的常量配置文件*/
 
-var _base_ip = "192.168.1.115";
+var _base_ip = "192.168.1.106";
 
 var _base_port = "8080";
 var _base_url = "http://" + _base_ip + ":" + _base_port;			//http:192.168.1.111:8080/
